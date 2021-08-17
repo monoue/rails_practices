1de4ea18110d090eabfc43badaddb9cc029ef9b5
monoue <like_a_newborn_baby@icloud.com>
2021-08-17 13:14:38 +0900

    lesson 9 done

--- rails_tutorial/environment/sample_app/app/models/user.rb
+++ rails_tutorial/environment/sample_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # 記憶トークン用
+  attr_accessor :remember_token
+
   # User モデル中では、右式の self を省略できる
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
@@ -9,9 +12,31 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,  length: { minimum: 6 }
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-             BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+               BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
