0c6d207eaf0434ecf33b3c9b54df922b394c5498
monoue <like_a_newborn_baby@icloud.com>
2021-08-27 15:22:49 +0900

    14.1 done

--- rails_tutorial/environment/sample_app/test/models/user_test.rb
+++ rails_tutorial/environment/sample_app/test/models/user_test.rb
@@ -1,9 +1,6 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
@@ -86,5 +83,16 @@ class UserTest < ActiveSupport::TestCase
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
+  end
+
+  test 'should follow and unfollow a user' do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
   end
 end
